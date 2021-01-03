5d8450ec8abe086312213af35ff25429665a4506
Jiakang Chen <58886570+jiakangchen005@users.noreply.github.com>
2021-01-03 17:57:32 -0500

    Update 2048.py

--- Kattis/2048.py
+++ Kattis/2048.py
@@ -1,13 +1,1 @@
-'''
-Firstly, learn how to play 2048 it will help and make sense of the problem
-
-Input: The input is always a valid game state of a 2048 puzzle. The first four lines of input, that each contains four integers, describe the 16 integers in the 4×4 grid of 2048 puzzle. 
-The j-th integer in the i-th line denotes the content of the cell located at the i-th row and the j-th cell. 
-For this problem, all integers in the input will be either {0, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024}. Integer 0 means an empty cell.
-The fifth line of input contains an integer 0, 1, 2, or 3 that denotes a left, up, right, or down move executed by the player, respectively.
-
-Output: Output four lines with four integers each. Two integers in a line must be separated by a single space. This describes the new state of the 4×4 grid of 2048 puzzle. 
-Again, integer 0 means an empty cell. 
-Note that in this problem, you can ignore the part from the 2048 puzzle where it introduces a new random tile with a value of either 2 or 4 in an empty spot of the board at the start of a new turn.
-'''
 
