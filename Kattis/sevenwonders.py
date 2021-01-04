f39da0a656b14d13c195519339c99547d46be752
Jiakang Chen <58886570+jiakangchen005@users.noreply.github.com>
2021-01-03 21:12:41 -0500

    Update sevenwonders.py

--- Kattis/sevenwonders.py
+++ Kattis/sevenwonders.py
@@ -1,1 +1,23 @@
+get = input()
+array = list(get)
 
+t = c = g = 0
+result = 0
+temp = 0
+
+for x in range(len(array)):
+	if (array[x] == "T"):
+		t = t + 1
+
+	elif (array[x] == "C"):
+		c = c + 1
+	
+	elif (array[x] == "G"):
+		g = g + 1
+
+	if (t and c and g > temp):
+		temp = temp + 1
+		result = result + 7
+	
+result = result + t ** 2 + c ** 2 + g ** 2
+print(result)
