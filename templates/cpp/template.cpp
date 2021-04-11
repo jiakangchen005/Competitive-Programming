d8b870bc6bed2637b389a17ac61138cd898a2ba8
Jiakang Chen <58886570+jiakangchen005@users.noreply.github.com>
2021-04-11 17:06:00 -0400

    Create template.cpp

---
+++ templates/cpp/template.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Declaration */
+
+
+
+/* Declaration */
+
+void solve() {
+	;
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+}
