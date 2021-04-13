aacbcd507cb2d39e44b56b7f594af8bf20e666d9
Jiakang Chen <58886570+jiakangchen005@users.noreply.github.com>
2021-04-13 12:23:06 -0400

    Update template.cpp

--- templates/cpp/template.cpp
+++ templates/cpp/template.cpp
@@ -1,5 +1,14 @@
 #include <bits/stdc++.h>
+
 using namespace std;
+
+#define cinl getline
+#define pb push_back
+
+typedef long double ld;
+typedef long long ll;
+
+const ll MOD = 1000000007; 
 
 /* Declaration */
 
@@ -15,4 +24,6 @@ int main() {
 	ios::sync_with_stdio(0);
 	cout.tie(0);
 	cin.tie(0);
+
+	// Start
 }
