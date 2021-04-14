6306870c08ddf8761bf9bde8e510f11201a7aafa
Jiakang Chen <58886570+jiakangchen005@users.noreply.github.com>
2021-04-14 16:51:47 -0400

    Create 4A.cpp

---
+++ Codeforces/cpp/4A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ld long double
+#define ll long long
+
+#define print(x) cout << x << "\n"
+#define cinl getline
+#define pb push_back
+
+const ll MOD = 1000000007;
+
+/* Declaration */
+
+int w;
+
+/* Declaration */
+
+void solve() {
+    ;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+
+    cin >> w;
+    if ((w % 2 == 0) && (w != 2)) {
+        print("YES");
+    } else {
+        print("NO");
+    }
+}
