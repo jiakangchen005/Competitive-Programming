20517a59bddd1a5cf5d0ccda9649d274c0881076
Jiakang Chen <58886570+jiakangchen005@users.noreply.github.com>
2021-05-27 08:30:48 -0400

    Update helper.py

--- templates/python/helper.py
+++ templates/python/helper.py
@@ -1,6 +1,19 @@
+import platform
 import os
 
-first = "main.py < in.txt"
-second = "input('Press enter to continue . . . ')"
-final = 'python ' + first + ' & python -c "' + second + '"'
-os.system(final)
+if (platform.system().lower() == "windows"):
+	a = "python main.py < in 2> error"
+	b = "python -c \"input('Press enter to continue . . . ')\""
+	temp = a + " && " + b
+	
+	os.system(temp)
+
+elif (platform.system().lower() == "linux"):
+	a = "python main.py < in 2> error"
+	b = "python -c \"input('Press enter to continue . . . ')\""
+	temp = a + " && " + b
+	
+	os.system(temp)
+
+else:
+	print("Error with operating system.")
