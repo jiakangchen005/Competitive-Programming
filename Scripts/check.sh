65f20ff6cd56e1382f45f7f945f149c774b95528
jiakangchen005 <jiakangchen005@gmail.com>
2022-02-16 14:36:27 -0500

    JC

---
+++ Scripts/check.sh
@@ -0,0 +1,1 @@
+g++ -std=c++17 -fsyntax-only -fmax-errors=5 -Wall -Wextra -Wno-memset-elt-size -Wno-sign-conversion -Wno-sign-compare -Wshadow main.cpp
---
+++ Scripts/clean.sh
@@ -0,0 +1,3 @@
+rm -rf main;
+rm -rf in;
+cp template.cpp main.cpp;
