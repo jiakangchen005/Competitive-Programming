4e183f24aa4a2d3ebfa5e7260803465031b8103a
jiakangchen005 <jiakangchen005@gmail.com>
2023-01-06 13:24:13 -0500

    usaco practice :)

---
+++ USACO/Contests/2015-2016/December/Silver/2_High_Card_Wins.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+void solve(istream& cin, ostream& cout) {
+    bool cards[2 * N + 1];
+
+    fill(cards, cards + (2 * N + 1), false);
+
+    for (int i = 0; i < N; i = i + 1) {
+        int card;
+        cin >> card;
+
+        cards[card] = true;
+    }
+
+    vector<int> elsie, bessie;
+
+    for (int i = 1; i <= 2 * N; i = i + 1) {
+        if (cards[i]) {
+            elsie.push_back(i);
+        } else {
+            bessie.push_back(i);
+        }
+    }
+
+    int j = -1, points = 0;
+
+    for (int i = 0; i < elsie.size(); i = i + 1) {
+        while (j + 1 < bessie.size()) {  
+            j = j + 1;
+
+            if (elsie[i] < bessie[j]) {
+                points = points + 1;
+                break;
+            }
+        }
+    }
+
+    cout << points << "\n";
+}
+
+int main() {
+    ifstream cin("highcard.in");
+    ofstream cout("highcard.out", ios::ate);
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+
+    solve(cin, cout);
+}
